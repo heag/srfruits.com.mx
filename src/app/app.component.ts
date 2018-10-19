45c0e8e9676e86e0b5fe87553d505a40e642486a
heag <39012199+heag@users.noreply.github.com>
2018-10-19 17:05:18 +0200

    SECCION OFERTA CON DATOS NO REALES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { IEnvironment, ENVIRONMENT } from '../environments/common';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frutas-alvarez';
 
-  constructor(@Inject(ENVIRONMENT) public environment: IEnvironment){
+  constructor(@Inject(ENVIRONMENT) public environment: IEnvironment,
+              private router: Router){
 
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      window.scrollTo(0, 0)
+  });
   }
 }
