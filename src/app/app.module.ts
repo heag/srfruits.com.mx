37c912cd96aabe2bfb9d2935766e4913e7b329df
heag <39012199+heag@users.noreply.github.com>
2019-01-20 20:10:05 +0100

    Try fix routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -85,7 +85,7 @@ const routes: Routes = [
   ],
   imports: [...angularCoreModules,
   ...materialModules,
-  RouterModule.forRoot(routes)
+  RouterModule.forRoot(routes, {useHash: false})
   // TranslateModule.forRoot({
   //   loader: {
   //     provide: TranslateLoader,
