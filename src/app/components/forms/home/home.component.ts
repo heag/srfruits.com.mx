cc6f5b783ebc3a9feccda65594b6b391af1e5b40
heag <39012199+heag@users.noreply.github.com>
2019-02-03 22:48:48 +0100

    QUITAR IMAGENES DE COPYRIGHT

--- src/app/components/forms/home/home.component.ts
+++ src/app/components/forms/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
     config.interval = 5000;
-    config.wrap = false;
+    config.wrap = true;
     config.keyboard = false;
     config.pauseOnHover = false;
   }
